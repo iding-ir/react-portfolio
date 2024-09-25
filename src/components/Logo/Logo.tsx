3697de1a679ec3535adec1c8488914a6425fae52
Aydin Ghane Kh. <aydin.ghane.kh@gmail.com>
2024-09-25 21:48:08 +0200

    Link logo to home page

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,10 +1,14 @@
-import Image from "../../assets/icon-logo.svg";
+import { Link } from "react-router-dom";
+import { URLS } from "../../routes/urls";
 import styles from "./Logo.module.scss";
+import Image from "../../assets/icon-logo.svg";
 
 export const Logo = () => {
   return (
     <div className={styles.container}>
-      <Image />
+      <Link to={URLS.HOME}>
+        <Image />
+      </Link>
     </div>
   );
 };
