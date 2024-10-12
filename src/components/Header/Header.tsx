e9364995213be216cd8ee84a5e284f9e41aec4c2
Aydin Ghane Kh. <aydin.ghane.kh@gmail.com>
2024-10-12 19:35:20 +0200

    Bugfix for Header component animation

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,9 @@
 import styles from "./Header.module.scss";
 
 export const Header = ({ title }: { title: string }) => {
-  return <header className={styles.container}>{title}</header>;
+  return (
+    <header key={title} className={styles.container}>
+      {title}
+    </header>
+  );
 };
