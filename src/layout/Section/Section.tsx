e5ebf66814ab398985dfa15c76aadda1af704d64
Aydin Ghane Kh. <aydin.ghane.kh@gmail.com>
2025-06-07 23:14:48 +0200

    Change transition mechanism

--- src/layout/Section/Section.tsx
+++ src/layout/Section/Section.tsx
@@ -1,6 +1,7 @@
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useRef } from "react";
+import { useLocation } from "react-router-dom";
+import { v4 as uuid } from "uuid";
 
-import { Transition } from "../../components/Transition";
 import { Wave } from "../../components/Wave";
 import styles from "./Section.module.scss";
 
@@ -11,11 +12,18 @@ export const Section = ({
   children: ReactNode;
   ariaDescribedBy: string;
 }) => {
+  const location = useLocation();
+  const ref = useRef<string>(uuid());
+
+  useEffect(() => {
+    ref.current = uuid();
+  }, [location]);
+
   return (
     <section className={styles.container} aria-describedby={ariaDescribedBy}>
-      {children}
-
-      <Transition />
+      <div key={ref.current} className={styles.children}>
+        {children}
+      </div>
 
       <Wave />
     </section>
